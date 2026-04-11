6b6fdd9522e0c2cb5f0d2a4731c3c61a992fe315
Oleg Martynov <fjolne.yngling@gmail.com>
2026-04-11 22:35:29 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems =
+        f:
+        nixpkgs.lib.foldl'
+          (
+            acc: system:
+            nixpkgs.lib.recursiveUpdate acc (
+              nixpkgs.lib.mapAttrs (_: value: { "${system}" = value; }) (f system)
+            )
+          )
+          { }
+          systems;
+    in
+    forAllSystems (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { allowUnfree = true; };
+        };
+      in
+      {
+        devShells = with pkgs; {
+          default = mkShell {
+            packages = [
+              # python
+              uv
+              # rust
+              rustup
+              clang
+              # node
+              nodejs
+              typescript
+            ];
+            # fix for python libs, e.g. numpy
+            LD_LIBRARY_PATH = lib.makeLibraryPath [
+              zlib
+              stdenv.cc.cc.lib
+            ];
+          };
+        };
+      });
+}
